888ba67b9a3b9e875602d22e16dbb04bce1db491
Branko Zivanovic <zivanovic.b1@hotmail.com>
2017-04-19 11:27:04 +0200

    validation

--- client/src/auth/RegisterEmailField.js
+++ client/src/auth/RegisterEmailField.js
@@ -5,46 +5,63 @@ export default class RegisterNameField extends Component{
 
   state = {
     value: '',
-    validated: false
+    error: ''
   }
 
   handleChange = event => {
     const { target: { value }} = event
+    this.setState({value})
+  }
 
-    if(value.length && value.includes('@')){
-      if(isEmail(value)){
-        this.setState({validated:true})
-      }else{
-        this.setState({validated:false})
-      }
-      this.setState({value})
-    }else{
-      this.setState({value: ''})
+  onValueChange(value){
+    const error = this.getErrorMessage(value)
+    this.setState({value,error})
+  }
+
+  getErrorMessage(value){
+    if(!value.length){
+      return `Enter email address please!`
+    }
+    if(!isEmail(value)){
+      return 'Email address is not valid. Please try again.'
     }
   }
 
   handleBlur = event => {
     const {value} = this.state
-
-    if(value !== ''){
-      if(isEmail(value)){
-        this.setState({validated:true})
-      }else{
-        this.setState({validated:false})
-      }
-    }
+    this.onValueChange(value)
   }
 
 
   render(){
+    const {error} = this.state
     return(
       <div>
-        <p>Email</p>
-        <input
-          type="email"
-          placeholder="e.g., zivanovic.b1@hotmail.com"
-          onChange={this.handleChange}
-          onBlur={this.handleBlur}/>
+        { error ? (
+          <div>
+            <p>Email</p>
+            <input
+              type="email"
+              placeholder="e.g., zivanovic.b1@hotmail.com"
+              onChange={this.handleChange}
+              onBlur={this.handleBlur}
+            />
+            <div className="fieldTip">
+              {error}
+            </div>
+          </div>
+
+        ) : (
+          <div>
+            <p>Email</p>
+            <input
+              type="email"
+              placeholder="e.g., zivanovic.b1@hotmail.com"
+              onChange={this.handleChange}
+              onBlur={this.handleBlur}
+            />
+          </div>
+        )}
       </div>
     )
   }
--- client/src/auth/RegisterForm.js
+++ client/src/auth/RegisterForm.js
@@ -9,16 +9,19 @@ import PasswordField from './PasswordField';
 
 export default class RegisterForm extends Component{
 
+  state = {
+    name: false,
+    email: false,
+    password: false
+  }
+
   render(){
 
     return(
       <form>
-
         <NameField/>
         <EmailField/>
         <PasswordField/>
-
-        
 
         <p className="terms">
           By proceeding to create your account and use Trello, you are agreeing to our Terms of Service and Privacy Policy. If you do not agree, you cannot use Trello.
